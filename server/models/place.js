2ee6ac2f8bb3edccad929628c960278c41ba4937
rachidusma <usmarachid9@gmail.com>
2020-07-13 13:28:23 +0100

    back updat

--- server/models/place.js
+++ server/models/place.js
@@ -1,34 +1,147 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+//const { min } = require("~/static/plugins/moment/moment");
 // const geocoder = require("../utils/geocoder");
 
 const PlaceSchema = new mongoose.Schema({
   beskreving: {
-    type: String,
-    required: [true, 'Please add a store ID'],
-    unique: true,
-    trim: true,
-    maxlength: [10, 'Store ID must be less than 10 chars']
+    type: String
   },
-  address: {
-    type: String,
-    required: [true, 'Please add an address']
+  bildgalleri: {
+    type: Array,
+    default: []
+  },
+  cover: {
+    type: Array,
+    default: []
+  },
+  Epost: {
+    type: String
+  },
+  prisperdag: {
+    type: Number,
+    min: 0
+  },
+  prisperhelg: {
+    type: Number,
+    min: 0
+  },
+  prisperlanghelg: {
+    type: Number,
+    min: 0
+  },
+  prispervecka: {
+    type: Number,
+    min: 0
+  },
+  prispermanad: {
+    type: Number,
+    min: 0
+  },
+  prioteradpris: {
+    type: Number,
+    min: 0
+  },
+  egenskaper: {
+    type: Array,
+    default: []
+  },
+  yta: {
+    type: Number,
+    min: 0
+  },
+  placering: {
+    type: String
+  },
+  stad: {
+    type: String
+  },
+  plats: {
+    type: String
   },
   location: {
     type: {
       type: String,
-      enum: ['Point']
+      enum: ["Point"]
     },
     coordinates: {
       type: [Number],
-      index: '2dsphere'
+      index: "2dsphere"
     },
     formattedAddress: String
+  },
+  kategori: {
+    type: Array,
+    default: []
+  },
+  planritning: {
+    type: Array,
+    default: []
+  },
+  minstahyresperiod: {
+    type: String
+  },
+  langstahyresperiod: {
+    type: String
+  },
+  fasta: {
+    type: Boolean
+  },
+  butik: {
+    type: Boolean
+  },
+  mat: {
+    type: Boolean
+  },
+  event: {
+    type: Boolean
+  },
+  SasongBoxen: {
+    type: String
+  },
+  hemsida: {
+    type: String
+  },
+  centrumtextarea: {
+    type: String
+  },
+  oppettider: [
+    {
+      day: String,
+      oppettider: String,
+      times: [
+        {
+          opening: String,
+          closing: String
+        }
+      ]
+    }
+  ],
+  timezone: {
+    type: String
+  },
+  vagvisningen: {
+    type: String
+  },
+  centrumgalleri: {
+    type: String
+  },
+  fran: {
+    type: Date
+  },
+  till: {
+    type: Date
+  },
+  kontaktperson: {
+    type: String
+  },
+  expiry: {
+    type: Date
   },
   createdAt: {
     type: Date,
     default: Date.now
   }
-})
+});
 
 // Geocode & create location
 /* PlaceSchema.pre("save", async function(next) {
@@ -44,4 +157,4 @@ const PlaceSchema = new mongoose.Schema({
   next();
 }); */
 
-module.exports = mongoose.model('Place', PlaceSchema)
+module.exports = mongoose.model("Place", PlaceSchema);
