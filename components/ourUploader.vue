8e930438a92ee86a77fb003b24317ceb29928cef
MohammedAyman2018 <emidobibo20@gmail.com>
2020-07-15 15:22:09 +0200

    Add drag and drop

--- components/ourUploader.vue
+++ components/ourUploader.vue
@@ -2,12 +2,19 @@
   <div>
     <b-row>
       <b-col v-for="input in inputs" :key="input" cols="3">
-        <div :data-id="`input-${input}`" class="input-group--wrapper">
+        <div
+          :data-id="`input-${input}`"
+          class="input-group--wrapper"
+          @drop="dropped"
+          @dragenter="dragIn"
+          @dragleave="dragOut"
+          @dragover="allowDrop"
+        >
           <button type="button" :data-id="`input-${input}`" class="close" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
 
-          <input type="file" :data-id="`input-${input}`">
+          <input type="file" accept="image/*" :data-id="`input-${input}`">
 
           <img :data-id="`input-${input}`">
         </div>
@@ -20,7 +27,8 @@
 export default {
   data () {
     return {
-      inputs: 1
+      inputs: 1,
+      files: 0
     }
   },
   watch: {
@@ -34,6 +42,58 @@ export default {
     this.initalize()
   },
   methods: {
+    dropped (event) {
+      event.preventDefault()
+
+      event.target.classList.remove('drag')
+
+      const vm = this
+      const inputs = document.querySelectorAll('.input-group--wrapper input')
+      const delBtns = document.querySelectorAll('.input-group--wrapper button.close')
+      const images = document.querySelectorAll('.input-group--wrapper img')
+
+      const wrapper = event.target
+
+      const input = [...inputs].find(x => x.getAttribute('data-id') === wrapper.getAttribute('data-id'))
+      const btn = [...delBtns].find(x => x.getAttribute('data-id') === wrapper.getAttribute('data-id'))
+
+      input.files = event.dataTransfer.files
+      const imageSrc = URL.createObjectURL(input.files[0])
+      const exactImg = [...images].find(img => img.getAttribute('data-id') === input.getAttribute('data-id'))
+      const exactDelBtn = [...delBtns].find(btn => btn.getAttribute('data-id') === input.getAttribute('data-id'))
+
+      exactDelBtn.style.display = 'block'
+      exactImg.style.display = 'block'
+      exactImg.setAttribute('src', imageSrc)
+      vm.files++
+      vm.inputs = vm.files + 1
+
+      btn.onclick = function (e) {
+        e.stopPropagation()
+        const input = [...inputs].find(i => i.getAttribute('data-id') === btn.getAttribute('data-id'))
+        const img = [...images].find(i => i.getAttribute('data-id') === btn.getAttribute('data-id'))
+        input.value = ''
+
+        img.removeAttribute('src')
+        img.style.display = 'none'
+        btn.style.display = 'none'
+        if (vm.inputs > 1) {
+          vm.inputs = vm.inputs - 1
+        }
+      }
+    },
+    allowDrop (event) {
+      event.preventDefault()
+      event.target.classList.add('drag')
+    },
+    dragIn (event) {
+      event.preventDefault()
+      event.target.classList.add('drag')
+    },
+    dragOut (event) {
+      event.preventDefault()
+      event.target.classList.remove('drag')
+    },
     initalize () {
       const vm = this
       const wrappers = document.querySelectorAll('.input-group--wrapper')
@@ -43,7 +103,6 @@ export default {
 
       wrappers.forEach((w) => {
         const id = w.getAttribute('data-id')
-        console.log(id)
         w.onclick = function () {
           [...inputs].find(i => i.getAttribute('data-id') === id).click()
         }
@@ -56,11 +115,13 @@ export default {
           const exactImg = [...images].find(img => img.getAttribute('data-id') === i.getAttribute('data-id'))
           const exactDelBtn = [...delBtns].find(btn => btn.getAttribute('data-id') === i.getAttribute('data-id'))
 
-          console.log(exactDelBtn)
           exactDelBtn.style.display = 'block'
           exactImg.style.display = 'block'
           exactImg.setAttribute('src', imageSrc)
-          vm.inputs = vm.inputs + 1
+          if (i.value !== '') {
+            vm.files++
+            vm.inputs = vm.files + 1
+          }
         }
       })
 
@@ -74,8 +135,9 @@ export default {
           img.removeAttribute('src')
           img.style.display = 'none'
           btn.style.display = 'none'
-          input.style.display = 'block'
-          vm.inputs = vm.inputs - 1
+          if (vm.inputs > 1) {
+            vm.files--
+          }
         }
       })
     }
@@ -90,13 +152,17 @@ export default {
   height: 150px
   position: relative
   overflow: hidden
-  font-family: "Font Awesome 5 Free" !important
-  &::after
+  margin-bottom: 10px
+  background: white
+  transition: all 0.4s ease
+  &:hover, &.drag
+    background: #eee
+  &::before
+    font-family: "Font Awesome 5 Free"
+    font-weight: 900
+    content: "\f093"
     transition: all 0.4s ease
-    background-color: #eeeeee
-    border: 2px solid #ddd
-    border-radius: 5px
-    content: "\f093" !important
+    border: 2px dashed #ddd
     display: flex
     justify-content: center
     align-items: center
@@ -106,10 +172,11 @@ export default {
     left: 0
     width: 100%
     height: 100%
+
   button.close
     position: absolute
     top: 0
-    right: 0
+    right: 10px
     display: none
     z-index: 4
     color: white
