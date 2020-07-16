f6d7a2a23aaf92b2ffb90663c868d0a3c65728c9
rachidusma <usmarachid9@gmail.com>
2020-07-16 12:16:49 +0100

    get all users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,11 @@
 const auth = require("../middleware/auth");
-const { getUser, addUser } = require("../controller/users");
+const { getUser, addUser, getAllUsers } = require("../controller/users");
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 
 router.get("/me", auth, getUser);
-
 router.post("/", addUser);
+router.get("/all", getAllUsers);
 
 module.exports = router;
