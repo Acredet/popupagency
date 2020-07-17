b450ea5ffcaaf98fab37a2e04f7053346b6dbb98
MohammedAyman2018 <emidobibo20@gmail.com>
2020-07-17 12:28:34 +0200

    Add eslintignore, addUser page and generate links in sidebar dynamically

---
+++ pages/admin/listings/tags.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <h1>Under construction</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddUser',
+  layout: 'admin'
+}
+</script>
+
+<style>
+
+</style>
