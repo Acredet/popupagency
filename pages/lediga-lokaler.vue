3a50fa53ce1aaa3a4e50317a4b0c11e825c0835d
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-14 14:26:37 +0200

    feat: remove any useless actions from the store and make the page just get the data from the store to display it
    body:
    we don't want this page to create any requests to the server
    so we just get the data from the store and the store doesn't make requests just for the first time

--- pages/lediga-lokaler.vue
+++ pages/lediga-lokaler.vue
@@ -1,160 +1,159 @@
 <template>
-  <div style="overflow-x: hidden" class="mt-3">
-    <loading :state="loadingState" />
+	<div style="overflow-x: hidden" class="mt-3">
+		<filter-sidebar />
 
-    <filter-sidebar />
+		<b-row class="mt-2">
+			<!-- Start filters Bar -->
+			<b-col cols="12">
+				<filter-bar
+					@changeMapCenter="center = $event"
+					@changeLayout="layout = $event"
+				/>
+			</b-col>
+			<!-- End filters Bar -->
 
-    <b-row class="mt-2">
-      <!-- Start filters Bar -->
-      <b-col cols="12">
-        <filter-bar @changeMapCenter="center = $event" @changeLayout="layout = $event" />
-      </b-col>
-      <!-- End filters Bar -->
+			<!-- Start Listings -->
+			<b-col
+				cols="12"
+				:md="layout === $t('ledigaLokaler.map') ? 6 : 12"
+				class="wrapper"
+			>
+				<b-container class="h-100">
+					<ListingsGrid :layout="layout" :cards="cards" />
+				</b-container>
+			</b-col>
+			<!-- End Listings -->
 
-      <!-- Start Listings -->
-      <b-col cols="12" :md="layout === $t('ledigaLokaler.map') ? 6 : 12" class="wrapper">
-        <b-container class="h-100">
-          <ListingsGrid :layout="layout" :cards="cards" :loading-cards="loadingCards" />
-        </b-container>
-      </b-col>
-      <!-- End Listings -->
+			<!-- Start Map -->
+			<b-col
+				v-if="layout === $t('ledigaLokaler.map')"
+				cols="12"
+				md="6"
+				class="map-wrapper d-md-flex"
+			>
+				<client-only>
+					<GMap
+						:key="renderKey"
+						:all-places="cards"
+						:center="center"
+						@setCenter="layout = $t('ledigaLokaler.map')"
+					/>
+				</client-only>
+			</b-col>
+			<!-- End Map -->
+		</b-row>
 
-      <!-- Start Map -->
-      <b-col v-if="layout === $t('ledigaLokaler.map')" cols="12" md="6" class="map-wrapper d-md-flex">
-        <client-only>
-          <GMap :key="renderKey" :all-places="cards" :center="center" @setCenter="layout = $t('ledigaLokaler.map')" />
-        </client-only>
-      </b-col>
-      <!-- End Map -->
-    </b-row>
+		<!-- Strat Toggle Layout in small screens -->
+		<div
+			class="mobile-btns px-3 d-md-none position-fixed d-flex justify-content-between align-items-center"
+		>
+			<b-btn
+				pill
+				variant="dark"
+				class="w-50 mr-2"
+				@click="
+					layout === $t('ledigaLokaler.map')
+						? (layout = $t('ledigaLokaler.list'))
+						: (layout = $t('ledigaLokaler.map'));
+					refreshMap;
+				"
+			>
+				{{
+					layout === $t("ledigaLokaler.map")
+						? $t("ledigaLokaler.list")
+						: $t("ledigaLokaler.map")
+				}}
+				{{ $t("ledigaLokaler.view") }}
+			</b-btn>
 
-    <!-- Strat Toggle Layout in small screens -->
-    <div class="mobile-btns px-3 d-md-none position-fixed d-flex justify-content-between align-items-center">
-      <b-btn
-        pill
-        variant="dark"
-        class="w-50 mr-2"
-        @click="(layout === $t('ledigaLokaler.map')) ? layout = $t('ledigaLokaler.list') : layout = $t('ledigaLokaler.map'); refreshMap"
-      >
-        {{ (layout === $t('ledigaLokaler.map')) ? $t('ledigaLokaler.list') : $t('ledigaLokaler.map') }} {{ $t('ledigaLokaler.view') }}
-      </b-btn>
-
-      <b-btn pill variant="dark" class="w-50">
-        Save search
-      </b-btn>
-    </div>
-    <!-- End Toggle Layout in small screens -->
-  </div>
+			<b-btn pill variant="dark" class="w-50"> Save search </b-btn>
+		</div>
+		<!-- End Toggle Layout in small screens -->
+	</div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import GMap from '@/components/lediga/Map'
-import filterSidebar from '@/components/lediga/fitlerSidebar'
-import filterBar from '@/components/lediga/filterBar'
+import { mapGetters } from "vuex";
+import GMap from "@/components/lediga/Map";
+import filterSidebar from "@/components/lediga/fitlerSidebar";
+import filterBar from "@/components/lediga/filterBar";
 
 export default {
-  name: 'LedigaLokaler',
-  components: {
-    GMap,
-    filterBar,
-    filterSidebar
-  },
-  data () {
-    return {
-      center: { lat: 59.334591, lng: 18.06324 },
-      loadingState: false,
-      loadingCards: false,
-      layout: this.$t('ledigaLokaler.list')
-    }
-  },
-  computed: {
-    ...mapGetters({
-      renderKey: 'renderKey',
-      cards: 'cards'
-    })
-  },
-  mounted () {
-    // this.loadingCards = true
-    this.loadingState = true
-
-    if (window.innerWidth >= '768') {
-      this.layout = this.$t('ledigaLokaler.map')
-    }
-
-    // this.loadingCards = false
-    this.loadingState = false
-  },
-  async created () {
-    this.loadingState = true
-    this.loadingCards = true
-    await this.getTags()
-    await this.getRegions()
-    await this.getCategories()
-    await this.getListings()
-    this.loadingCards = false
-  },
-  methods: {
-    ...mapActions({
-      refreshMap: 'changeSidebarRenderKey',
-      getListings: 'getListings',
-      getRegions: 'getRegions',
-      getCategories: 'getCategories',
-      sortCards: 'sortCards',
-      getTags: 'getTags'
-    })
-  }
-}
+	name: "LedigaLokaler",
+	components: {
+		GMap,
+		filterBar,
+		filterSidebar,
+	},
+	data() {
+		return {
+			center: { lat: 59.334591, lng: 18.06324 },
+			layout: this.$t("ledigaLokaler.list"),
+		};
+	},
+	computed: {
+		...mapGetters({
+			renderKey: "renderKey",
+			cards: "cards",
+		}),
+	},
+	mounted() {
+		if (window.innerWidth >= "768") {
+			this.layout = this.$t("ledigaLokaler.map");
+		}
+	},
+	methods: {},
+};
 </script>
 
 <style scoped>
 .mobile-btns {
-  bottom: 5px;
-  left: 50%;
-  width: 100%;
-  transform: translate(-50%, -50%);
-  z-index: 9999;
+	bottom: 5px;
+	left: 50%;
+	width: 100%;
+	transform: translate(-50%, -50%);
+	z-index: 9999;
 }
 @media (max-width: 767.98px) {
-  .map-wrapper {
-    padding: 0 !important;
-    margin: 0 !important;
-    display: block !important;
-    position: fixed;
-    z-index: 25;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
+	.map-wrapper {
+		padding: 0 !important;
+		margin: 0 !important;
+		display: block !important;
+		position: fixed;
+		z-index: 25;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
 }
 
 .dropdown-header {
-  padding: 0 !important;
-  padding-bottom: 10px !important;
+	padding: 0 !important;
+	padding-bottom: 10px !important;
 }
 
 .wrapper {
-  max-height: 100vh;
-  overflow-y: scroll;
+	max-height: 100vh;
+	overflow-y: scroll;
 }
 
 .vue-map-container {
-  height: 100vh;
-  width: 100%;
+	height: 100vh;
+	width: 100%;
 }
 
 .choices,
 .choices * {
-  height: auto !important;
+	height: auto !important;
 }
 
 span ~ button.list-group-item {
-  background-color: #ddd !important;
+	background-color: #ddd !important;
 }
 
 .custom-tab,
 .custom-tab div {
-  height: 300px;
+	height: 300px;
 }
 </style>
