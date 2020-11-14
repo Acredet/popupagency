f49f4322ae9cff103558d802d2a275215c05a4ae
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-14 14:09:28 +0200

    feat: add availablePopups route to be used for the available popups page to fetch the required data body: the availablePopups popup page should focus more on the ui part not creating many requests to the server

---
+++ server/routes/availablePopups.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const Place = require("../models/place");
+const Region = require("../models/region");
+const Tag = require("../models/tag");
+const Category = require("../models/category");
+const Centrum = require("../models/centrum");
+
+router.get("/", async (req, res) => {
+	try {
+		const regions = await Region.find({}, { __v: 0 });
+		const tags = await Tag.find({}, { __v: 0 });
+		const categories = await Category.find({}, { __v: 0 });
+		const places = await Place.find({ draft: false }, { __v: -0 });
+		const listings = [];
+		for (let place of places) {
+			let newPlace = { ...place.toObject() };
+			const listingRegion = regions.find(
+				(region) => region.name.en === place.stad.en
+			);
+			if (listingRegion.centrum) {
+				const centrum = await Centrum.findById(listingRegion.centrum);
+				if (centrum) {
+					newPlace.hemsida = centrum.hemsida;
+					newPlace.centrumgalleri = centrum.centrumgalleri;
+					newPlace.centrumtextarea = centrum.centrumtextarea;
+					newPlace.oppettider = centrum.oppettider;
+					newPlace.location = centrum.routeGuidance;
+				}
+			}
+			listings.push(newPlace);
+		}
+		res.status(200).json({ listings, tags, categories, regions });
+	} catch (e) {
+		res.status(401).json({
+			error: true,
+			message: "error fetching the available popups",
+		});
+	}
+});
+module.exports = router;
