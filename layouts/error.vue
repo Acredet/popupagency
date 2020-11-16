52164f9413f74b8afa2374241fbcecdbc55ee4a0
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-16 11:13:16 +0200

    config: make the error page return to the home page based on the user language preference.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="wrapper-page">
-    <div class="card">
-      <div class="card-block">
-        <div class="ex-page-content text-center">
-          <h1 class="text-dark">
-            404!
-          </h1>
-          <h4 class="">
-            Sorry, page not found
-          </h4><br>
+	<div class="wrapper-page">
+		<div class="card">
+			<div class="card-block">
+				<div class="ex-page-content text-center">
+					<h1 class="text-dark">404!</h1>
+					<h4 class="">Sorry, page not found</h4>
+					<br />
 
-          <nuxt-link class="btn btn-info mb-5 waves-effect waves-light" to="/">
-            <i class="mdi mdi-home" /> Back to Home
-          </nuxt-link>
-        </div>
-      </div>
-    </div>
-  </div>
+					<nuxt-link
+						class="btn btn-info mb-5 waves-effect waves-light"
+						:to="localePath('/')"
+					>
+						<i class="mdi mdi-home" /> Back to Home
+					</nuxt-link>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'ErrorPage',
-  layout: 'auth'
-}
+	name: "ErrorPage",
+	layout: "auth",
+};
 </script>
