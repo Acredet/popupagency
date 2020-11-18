84be73e6c32c9f8530a60055a328390a4332e34e
Mostafa Salama <mostafatourad789@gmail.com>
2020-11-18 12:18:31 +0200

    fix: update the path for storing images on the backend when upload files to fix various erros
    body:
    the upload middleware throw an errors when creating new places.

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,20 +1,19 @@
-const multer = require('multer')
-
+const multer = require("multer");
+const path = require("path");
 const storage = multer.diskStorage({
-  destination (req, file, cb) {
-    cb(null, `${__dirname}/../../.nuxt/dist/client/img/`)
-  },
-  filename (req, file, cb) {
-    cb(null, `${Date.now()}-${file.fieldname}-${file.originalname}`)
-  }
-})
+	destination(req, file, cb) {
+		cb(null, path.join(__dirname, "..", "client", "img"));
+	},
+	filename(req, file, cb) {
+		cb(null, `${Date.now()}-${file.fieldname}-${file.originalname}`);
+	},
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
-function uploadImage (req, res) {
-  
-  return res.status(200).json(req.file.filename)
+function uploadImage(req, res) {
+	return res.status(200).json(req.file.filename);
 }
 
-module.exports.multer = upload
-module.exports.uploadImage = uploadImage
+module.exports.multer = upload;
+module.exports.uploadImage = uploadImage;
