8bd98a0cc20f9ba640059352605b766680757524
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-12 19:05:15 +0200

    fix: Check if the admin files are in the dom before insterting them.

--- mixins/AdminPanelDependancies.js
+++ mixins/AdminPanelDependancies.js
@@ -5,15 +5,27 @@ exports.AdminPanelDependancies = {
 		this.$forceUpdate();
 	},
 	updated() {
-		this.loadScript("/js/adminfiles.js");
+		const filesInThere = document.querySelectorAll(
+			'script[src="/js/adminfiles.js"]'
+		);
+		if (filesInThere.length === 0) {
+			this.loadScript("/js/adminfiles.js");
+		} else {
+			filesInThere.forEach((file) => file.parentNode.removeChild(file));
+			this.loadScript("/js/adminfiles.js");
+		}
 		console.log("updated");
 	},
 	methods: {
 		loadScript(src) {
-			let script = document.createElement("script");
-			script.src = src;
-			script.async = false;
-			document.body.insertBefore(script, document.body.firstChild);
+			try {
+				let script = document.createElement("script");
+				script.src = src;
+				script.async = false;
+				document.body.insertBefore(script, document.body.firstChild);
+			} catch (e) {
+				loadScripts(src);
+			}
 		},
 	},
 	head() {
