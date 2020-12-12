51728aacdb2e33333deadd256db93ffd9d234c21
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-12 20:20:11 +0200

    feat: Add savedFilters property in user model and make route for it].

--- server/models/user.js
+++ server/models/user.js
@@ -1,56 +1,62 @@
-const jwt = require('jsonwebtoken')
-const Joi = require('joi')
-const mongoose = require('mongoose')
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 1024
-  },
-  role: {
-    type: String,
-    default: 'searcher'
-  },
-  fav: {
-    type: [String],
-    default: []
-  }
-},
-{ timestamps: { createdAt: 'created_at' } })
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+	{
+		savedFilters: {
+			type: Array,
+			default: [],
+		},
+		name: {
+			type: String,
+			required: true,
+			minlength: 5,
+			maxlength: 50,
+		},
+		email: {
+			type: String,
+			required: true,
+			minlength: 5,
+			maxlength: 255,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 5,
+			maxlength: 1024,
+		},
+		role: {
+			type: String,
+			default: "searcher",
+		},
+		fav: {
+			type: [String],
+			default: [],
+		},
+	},
+	{ timestamps: { createdAt: "created_at" } }
+);
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign(
-    { _id: this._id, isAdmin: this.role === 'admin' },
-    process.env.jwtPrivateKey
-  )
-  return token
+	const token = jwt.sign(
+		{ _id: this._id, isAdmin: this.role === "admin" },
+		process.env.jwtPrivateKey
+	);
+	return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+	const schema = {
+		name: Joi.string().min(5).max(50).required(),
+		email: Joi.string().min(5).max(255).required().email(),
+		role: Joi.string(),
+		password: Joi.string().min(5).max(255).required(),
+	};
+	return Joi.validate(user, schema);
 }
 
-const User = mongoose.model('User', userSchema)
-
-function validateUser (user) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    role: Joi.string(),
-    password: Joi.string().min(5).max(255).required()
-  }
-  return Joi.validate(user, schema)
-}
-
-exports.User = User
-exports.validate = validateUser
+exports.User = User;
+exports.validate = validateUser;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const auth = require('../middleware/auth')
+const express = require("express");
+const auth = require("../middleware/auth");
 const {
-  getUser,
-  getUsersByRole,
-  addUser,
-  getAllUsers,
-  authenticate,
-  deleteUser,
-  updateUser,
-  changePassword
-} = require('../controller/users')
+	getUser,
+	getUsersByRole,
+	addUser,
+	getAllUsers,
+	authenticate,
+	deleteUser,
+	updateUser,
+	changePassword,
+	savedFilters,
+} = require("../controller/users");
 // const { User, validate } = require('../models/user')
-const router = express.Router()
+const router = express.Router();
 
-router.get('/me', auth, getUser)
-router.post('/', addUser)
-router.get('/all', auth, getAllUsers)
-router.post('/auth', authenticate)
-router.post('/logout', auth, (req, res) => res.status(200).json({ success: true }))
-router.delete('/:id', auth, deleteUser)
-router.patch('/:id', auth, updateUser)
-router.patch('/password/:id', auth, changePassword)
-router.get('/:role', auth, getUsersByRole)
-module.exports = router
+router.get("/me", auth, getUser);
+router.post("/", addUser);
+router.get("/all", auth, getAllUsers);
+router.post("/auth", authenticate);
+router.post("/logout", auth, (req, res) =>
+	res.status(200).json({ success: true })
+);
+router.delete("/:id", auth, deleteUser);
+router.patch("/:id", auth, updateUser);
+router.patch("/password/:id", auth, changePassword);
+router.get("/:role", auth, getUsersByRole);
+router.post("/filters/add", savedFilters);
+module.exports = router;
