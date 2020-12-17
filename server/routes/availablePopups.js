028c567d88e8583b0efdaba3c4a446fd0929e84a
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-17 19:54:20 +0200

    refactor: send title with listing obj

--- server/routes/availablePopups.js
+++ server/routes/availablePopups.js
@@ -22,6 +22,7 @@ router.get("/", async (req, res) => {
 			if (listingRegion.centrum) {
 				const centrum = await Centrum.findById(listingRegion.centrum);
 				if (centrum) {
+					newPlace.centurmTitle = centrum.title;
 					newPlace.hemsida = centrum.hemsida;
 					newPlace.centrumgalleri = centrum.centrumgalleri;
 					newPlace.centrumtextarea = centrum.centrumtextarea;
