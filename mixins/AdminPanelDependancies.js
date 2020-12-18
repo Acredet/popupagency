0c7bc2cdeae8e3582c934929074089705399c240
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-18 17:15:56 +0200

    fix: Improve loadScript function.

--- mixins/AdminPanelDependancies.js
+++ mixins/AdminPanelDependancies.js
@@ -4,27 +4,23 @@ exports.AdminPanelDependancies = {
 		this.loadScript("/js/adminfiles.js");
 		this.$forceUpdate();
 	},
-	updated() {
-		const filesInThere = document.querySelectorAll(
-			'script[src="/js/adminfiles.js"]'
-		);
-		if (filesInThere.length === 0) {
-			this.loadScript("/js/adminfiles.js");
-		} else {
-			filesInThere.forEach((file) => file.parentNode.removeChild(file));
-			this.loadScript("/js/adminfiles.js");
-		}
-		console.log("updated");
-	},
 	methods: {
 		loadScript(src) {
+			const filesInThere = document.querySelectorAll(
+				'script[src="/js/adminfiles.js"]'
+			);
+
+			if (filesInThere.length !== 0) {
+				filesInThere.forEach((file) => file.parentNode.removeChild(file));
+			}
+
 			try {
 				let script = document.createElement("script");
 				script.src = src;
 				script.async = false;
 				document.body.insertBefore(script, document.body.firstChild);
 			} catch (e) {
-				loadScripts(src);
+				console.log(e);
 			}
 		},
 	},
