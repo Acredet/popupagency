375b064bbbb9ab4ce2ca58b7220c4e925e5078fb
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-26 20:31:12 +0200

    fix: use bforemoute hook instead of mounted.

--- mixins/AdminPanelDependancies.js
+++ mixins/AdminPanelDependancies.js
@@ -1,39 +1,39 @@
 /* eslint-disable no-sequences */
 exports.AdminPanelDependancies = {
-	mounted() {
-		this.loadScript("/js/adminfiles.js");
-		this.$forceUpdate();
-	},
-	methods: {
-		loadScript(src) {
-			const filesInThere = document.querySelectorAll(
-				'script[src="/js/adminfiles.js"]'
-			);
+  beforeMount() {
+    console.log("updated");
+    this.loadScript("/js/adminfiles.js");
+  },
+  methods: {
+    loadScript(src) {
+      const filesInThere = document.querySelectorAll(
+        'script[src="/js/adminfiles.js"]'
+      );
 
-			if (filesInThere.length !== 0) {
-				filesInThere.forEach((file) => file.parentNode.removeChild(file));
-			}
+      if (filesInThere.length !== 0) {
+        filesInThere.forEach((file) => file.parentNode.removeChild(file));
+      }
 
-			try {
-				let script = document.createElement("script");
-				script.src = src;
-				script.async = false;
-				document.body.insertBefore(script, document.body.firstChild);
-			} catch (e) {
-				console.log(e);
-			}
-		},
-	},
-	head() {
-		return {
-			link: [
-				{
-					href: "/css/admin-styles.css",
-					rel: "stylesheet",
-					type: "text/css",
-					body: true,
-				},
-			],
-		};
-	},
+      try {
+        let script = document.createElement("script");
+        script.src = src;
+        script.async = false;
+        document.body.insertBefore(script, document.body.firstChild);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  head() {
+    return {
+      link: [
+        {
+          href: "/css/admin-styles.css",
+          rel: "stylesheet",
+          type: "text/css",
+          body: true,
+        },
+      ],
+    };
+  },
 };
