9a0eb6524232624d7fe9fadd86aa438046c224f8
MohammedAyman2018 <emidobibo20@gmail.com>
2020-12-31 20:16:31 +0200

    feat: change number seperator to space.

--- mixins/utils/formatNo.js
+++ mixins/utils/formatNo.js
@@ -1,26 +1,26 @@
 exports.format = {
-	methods: {
-		format(num) {
-			const arr = String(num).split("").reverse();
-			const copy = [...arr];
-			const indexes = [];
+  methods: {
+    format(num) {
+      const arr = String(num).split("").reverse();
+      const copy = [...arr];
+      const indexes = [];
 
-			for (let i = 1; i <= arr.length; i++) {
-				if (i % 3 === 0) {
-					indexes.push(i);
-				}
-			}
+      for (let i = 1; i <= arr.length; i++) {
+        if (i % 3 === 0) {
+          indexes.push(i);
+        }
+      }
 
-			if (copy.length > 3) {
-				indexes.forEach((n, i) => copy.splice(n + i, 0, ","));
-			}
+      if (copy.length > 3) {
+        indexes.forEach((n, i) => copy.splice(n + i, 0, " "));
+      }
 
-			copy.reverse();
-			if (copy[0] === ",") {
-				copy.shift();
-			}
-			copy.join("");
-			return copy.join("");
-		},
-	},
+      copy.reverse();
+      if (copy[0] === ",") {
+        copy.shift();
+      }
+      copy.join("");
+      return copy.join("");
+    },
+  },
 };
